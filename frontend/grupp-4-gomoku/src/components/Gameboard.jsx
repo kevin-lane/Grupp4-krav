381821e7681a4d051bad306bed75486a1f9982fd
Kevin <kevinlane3@hotmail.com>
2023-10-26 14:14:23 +0200

    Game board done
    Game board done

--- frontend/grupp-4-gomoku/src/components/Gameboard.jsx
+++ frontend/grupp-4-gomoku/src/components/Gameboard.jsx
@@ -1,44 +1,37 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components';
-
-
-
+import json from '../../public/data.json';
 
 const array = []
 
 function Gameboard() {
+  const [board, setBoard] = useState([]);
 
-
-   const [board, setBoard] = useState([]);
-
+  console.log(json.board.tiles.map(index => index));
   let table = document.getElementById('gameboardtable');
-  let row = 16
-  let col = 16
-  for (let i = 0; i < col; i++) {
-    console.log("I: " + i);
-    for (let j = i; j < row; j++) {
-      console.log("J: " + j);
+  let row = json.board.rows;
+  let col = json.board.cols;
+  for (let i = 0; i < row; i++) {
+    for (let j = i; j < col; j++) {
       array.push(j)
-
     }
-
   }
+  console.log(json.board.tiles);
 
   return (
     <>
     <Div>
-
          {array.map((array, id) => (
-          <div className="container" key={id}>hej</div>
+          <div className="container" key={id}>
+            {/* {json.map((json, id) => (
+               <div key={id}>{json.board.tiles}</div>
+            ))} */}
+          </div>
         ))}
-
-
     </Div>
     </>
   )
 }
-
-
 
 export default Gameboard
 
@@ -48,9 +41,9 @@ grid-template-columns: repeat(16, auto);
 grid-template-rows: repeat(16, auto);
 display: grid;
 width: 100px;
-gap: 1px;
-
+gap: 2px;
 `
+
 // display: grid;
 // grid-template-columns: repeat(16, 1fr);
 // gap: 1px;
