fb2c7bcf9a6f0c39a1362bbc75cad39bedbbea0e
amariths <112472607+amariths@users.noreply.github.com>
2023-10-30 10:06:00 +0100

    change color on click board

--- frontend/grupp-4-gomoku/src/components/Gameboard.jsx
+++ frontend/grupp-4-gomoku/src/components/Gameboard.jsx
@@ -1,33 +1,64 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components';
-import json from '../../public/data.json';
+import json from './data.json';
 
 const array = []
 
 function Gameboard() {
   const [board, setBoard] = useState([]);
 
-  console.log(json.board.tiles.map(index => index));
+
+  console.log(json.board.tiles.map(index => (
+    <div>{index}</div>
+  )));
   let table = document.getElementById('gameboardtable');
-  let row = json.board.rows;
-  let col = json.board.cols;
+  let row = 8;
+  let col = 16;
   for (let i = 0; i < row; i++) {
-    for (let j = i; j < col; j++) {
-      array.push(j)
+    for (let j = 0; j < col; j++) {
+      array.push(json.board.tiles[j])
     }
   }
-  console.log(json.board.tiles);
+
+  const [bool, setBool] = useState([])
+  const [boo, setBoo] = useState(false)
+
+  const move = (index) => () => {
+
+
+    setBool(state => ({
+      ...state, // <-- copy previous state
+      [index]: !state[index], // <-- update value by index key
+    }));
+    setBoo(true)
+  };
+
+
+  const handle = (e) => {
+
+  }
+
 
   return (
     <>
     <Div>
-         {array.map((array, id) => (
-          <div className="container" key={id}>
-            {/* {json.map((json, id) => (
-               <div key={id}>{json.board.tiles}</div>
-            ))} */}
+          {array.map((array, index) => (
+            <div  className='container' onClick={handle}>
+          <div
+          key={index}
+          className={bool[index] && !boo ? "container1" : "container2"}
+          onClick={move(index)}
+
+          >
+
+          </div>
+
           </div>
         ))}
+        {/* {json.board.tiles.map(index => (
+    <div className='container'>{index}</div>
+  ))} */}
+
     </Div>
     </>
   )
@@ -37,11 +68,13 @@ export default Gameboard
 
 
 const Div = styled.div`
+
 grid-template-columns: repeat(16, auto);
 grid-template-rows: repeat(16, auto);
 display: grid;
 width: 100px;
 gap: 2px;
+
 `
 
 // display: grid;
