1338ff733b8c0fcd19b714a291116ede9a46bb3e
Kevin <kevinlane3@hotmail.com>
2023-10-30 16:11:52 +0100

    Tried to get X and O on the board
    Tried to get X and O on the board

--- frontend/grupp-4-gomoku/src/components/Gameboard.jsx
+++ frontend/grupp-4-gomoku/src/components/Gameboard.jsx
@@ -7,6 +7,13 @@ const array = []
 function Gameboard() {
   const [board, setBoard] = useState([]);
 
+  const [boards, setBoards] = useState([]);
+
+
+  useEffect(() => {
+      setBoards(json.board.tiles);
+  }, []);
+
   console.log(json.board.tiles.map(index => index));
   let table = document.getElementById('gameboardtable');
   let row = json.board.rows;
@@ -17,33 +24,86 @@ function Gameboard() {
     }
   }
   console.log(json.board.tiles);
+  console.log(array);
+
+  const [bool, setBool] = useState([])
+  const [boo, setBoo] = useState(false)
+  const [playerTurn, setPlayerTurn] = useState(null);
+
+
+  const move = (colindex, rowindex) => () => {
+
+    if(playerTurn === 1  ) {
+      json.board.tiles[rowindex][colindex] = 1
+      setPlayerTurn(2);
+    }
+    else if (playerTurn === 2 ) {
+      json.board.tiles[rowindex][colindex] = 2
+      setPlayerTurn(1);
+    }
+
+
+
+    setBoo(true)
+  };
+
+
+
 
   return (
     <>
-    <Div>
-         {array.map((array, id) => (
-          <div className="container" key={id}>
-            {/* {json.map((json, id) => (
-               <div key={id}>{json.board.tiles}</div>
-            ))} */}
-          </div>
-        ))}
-    </Div>
+    <Board>
+                 {/* Loopa igenom varje row i boards arrayen */}
+            {boards.map((row, rowIndex) => (
+                 // För varje row loopa igen om cols som ger cellerna.
+                row.map((col, colIndex) => (
+                    <Tile key={`${rowIndex}-${colIndex}`} >
+                        <Kryss onClick={move(colIndex, rowIndex)}> {col === 1 ? "X" : col === 2 ? "O" : ""}</Kryss>
+
+                    </Tile>
+                ))
+            ))}
+        </Board>
     </>
   )
 }
 
 export default Gameboard
 
-
 const Div = styled.div`
-grid-template-columns: repeat(16, auto);
-grid-template-rows: repeat(16, auto);
+grid-template-columns: repeat(17, auto);
+grid-template-rows: repeat(17, auto);
 display: grid;
 width: 100px;
 gap: 2px;
 `
 
-// display: grid;
-// grid-template-columns: repeat(16, 1fr);
-// gap: 1px;
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: red;
+  position: absolute;
+  border-radius: 50%;
+
+`
+const Board = styled.div`
+  display: grid;
+  grid-template-columns: repeat(17, 1fr);
+  gap: 0px;
+  width: 370.5px;
+  border: 1px solid black;
+`;
+
+const Tile = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+
+`;
+
+const Kryss = styled.p`
+color: red;
+margin: -6.5px ;
+margin-top: -10px;
+
+`
