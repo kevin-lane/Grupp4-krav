8e1c8ccd4a5299d76d70e8c28a23392b2fb74fdd
amariths <112472607+amariths@users.noreply.github.com>
2023-11-06 10:25:14 +0100

    ändrade json i backend

--- backend/src/routes/gomoku_routes.js
+++ backend/src/routes/gomoku_routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const gameController = require('../controllers/game_controller.js')
 
-router.get('/create_game', gameController.createGame);
+router.get('/create-game', gameController.createGame);
 
-router.get('/add_player', gameController.addPlayer);
+router.get('/add-player', gameController.addPlayer);
 
 router.get('/play', gameController.play);
 
